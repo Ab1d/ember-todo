f13aa4e7607f15fd838daf56bc71a190c74002e4
Your Real Name <Abid@FDLMC720.local>
2016-02-15 15:34:39 +0530

    first commit

---
+++ app/routes/todos.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+      model() {
+      return [
+        { text: "Create Ember app" },
+        { text: "Read Ember Igniter" },
+        { text: "Master Ember" }
+      ];
+    }
+});
